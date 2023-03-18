fe34fb41906b53dc79d04702ff86533df0a13af2
AlexanderIon <tov.sandro@mail.ru>
2023-03-18 13:10:07 +0300

    checking

---
+++ src/js/app.js
@@ -0,0 +1,110 @@
+import AddListCard from './widgetListTask.js';
+
+const board = document.querySelector('.board');
+const haveToDo = document.getElementById('haveToDo');
+const inProcessing = document.getElementById('inProcessing');
+const isDoneTask = document.getElementById('isDone');
+const myTasks = new AddListCard(haveToDo, 'have to');
+const myProcessingTasks = new AddListCard(inProcessing, 'processing');
+const myDoneTasks = new AddListCard(isDoneTask, 'done');
+myTasks.addList();
+myProcessingTasks.addList();
+myDoneTasks.addList();
+let itemActiv = false;
+let elemUnder;
+let itemActivBound;
+let shiftX;
+// console.log(haveToDo);
+// board.addEventListener('click', (e) => {
+
+// })
+const boardMouseOver = (e) => {
+  if (!itemActiv) return;
+  elemUnder = e.fromElement;
+  itemActiv.style.top = `${e.pageY - itemActiv.offsetHeight / 2}px`;
+  itemActiv.style.left = `${e.pageX - shiftX + itemActivBound.left}px`;
+};
+const boardMouseUp = () => {
+  // console.log('UP')
+  // console.log(elemUnder)
+  if (elemUnder === undefined) return;
+  if (elemUnder.parentNode.classList.contains('list')) {
+    elemUnder.parentNode.insertBefore(itemActiv, elemUnder);
+  } else {
+    console.log('Не попал на элемент');
+    // const avticList = elemUnder.closest('.container');
+  }
+
+  // console.log(elemUnder.classList);
+
+  itemActiv.classList.remove('dragger');
+  // elemUnder.parentNode.insertBefore(itemActiv,elemUnder);
+  itemActiv = undefined;
+  elemUnder = undefined;
+  document.documentElement.removeEventListener('mouseover', boardMouseOver);
+  document.documentElement.removeEventListener('mouseup', boardMouseUp);
+};
+
+board.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  //   console.log('FFFF');
+  itemActiv = e.target;
+  console.log(itemActiv.classList.contains('item-list'));
+  if (!itemActiv.classList.contains('item-list')) return;
+  itemActivBound = e.target.getBoundingClientRect();
+  shiftX = e.pageX;
+  //   console.log(itemActivBound);
+
+  itemActiv.classList.add('dragger');
+  document.documentElement.addEventListener('mouseover', boardMouseOver);
+  document.documentElement.addEventListener('mouseup', boardMouseUp);
+});
+
+window.addEventListener('beforeunload', () => {
+  const myHaveTo = Array.from(haveToDo.querySelectorAll('.item-list'));
+  const myInProsec = Array.from(inProcessing.querySelectorAll('.item-list'));
+  const myDone = Array.from(isDoneTask.querySelectorAll('.item-list'));
+  const myData = {};
+  myHaveTo.forEach((el, i) => {
+    myData[`${haveToDo.id}_${i}`] = el.textContent.substring(0, el.textContent.search('\n'));
+  });
+  myInProsec.forEach((el, i) => {
+    myData[`${inProcessing.id}_${i}`] = el.textContent.substring(0, el.textContent.search('\n'));
+  });
+  myDone.forEach((el, i) => {
+    myData[`${isDoneTask.id}_${i}`] = el.textContent.substring(0, el.textContent.search('\n'));
+  });
+  localStorage.setItem('myBoard', JSON.stringify(myData));
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const json = localStorage.getItem('myBoard');
+  let myBoard;
+  try {
+    myBoard = JSON.parse(json);
+  } catch (error) {
+    console.log(error);
+  }
+  if (myBoard) {
+    let insertList;
+
+    Object.keys(myBoard).forEach((key) => {
+      if (key.includes(haveToDo.id)) {
+        insertList = haveToDo.querySelector('.list');
+        insertList.insertAdjacentHTML('beforeend', myTasks.newItem(myBoard.key));
+
+        itemActiv = undefined;
+      } else if (key.includes(inProcessing.id)) {
+        insertList = inProcessing.querySelector('.list');
+        insertList.insertAdjacentHTML('beforeend', myProcessingTasks.newItem(myBoard.key));
+
+        itemActiv = undefined;
+      } else if (key.includes(isDoneTask.id)) {
+        insertList = isDoneTask.querySelector('.list');
+        insertList.insertAdjacentHTML('beforeend', myDoneTasks.newItem(myBoard.key));
+
+        itemActiv = undefined;
+      }
+    });
+  }
+});
---
+++ src/js/widgetListTask.js
@@ -0,0 +1,192 @@
+export default class AddListCard {
+  constructor(parentEl, title) {
+    this.parentEl = parentEl;
+    this.title = title;
+    this.actualItem = false;
+    this.addItem = false;
+    this.clikcBtn = this.clikcBtn.bind(this);
+    this.mouseDownOnList = this.mouseDownOnList.bind(this);
+    this.mouseUpOnList = this.mouseUpOnList.bind(this);
+    this.mouseOverItem = this.mouseOverItem.bind(this);
+    this.mouseOutItem = this.mouseOutItem.bind(this);
+    this.newItem = this.newItem.bind(this);
+  }
+
+  static get getHtlm() {
+    return `
+        <div class="container-for-card">
+            <div class="title">
+            Title
+            </div>
+            <div class="container-for-list">
+            <ul class="list">
+                
+            <li class='item-list'> TEST
+            <button class='delete-item'>X</button> </li>
+               
+
+            </ul>    
+            </div>
+            
+            <button class='btn-add-task'>
+            ADD another card
+            </button>
+            </div>
+        </div>     
+        `;
+  }
+
+  static get getTitle() {
+    return '.title';
+  }
+
+  static get getbtn() {
+    return '.btn-add-task';
+  }
+
+  static get getList() {
+    return '.list';
+  }
+
+  static get addItem() {
+    return `<li class='item-list'>
+    <button class='delete-item'>X</button> </li>
+            `;
+  }
+
+  static get getItem() {
+    return 'item-list';
+  }
+
+  static get getActive() {
+    return '.dragger';
+  }
+
+    newItem(textItem) {
+      return `<li class='item-list'>${textItem}
+      <button class='delete-item'>X</button> </li>
+              `;
+    }
+
+  addList() {
+    this.parentEl.insertAdjacentHTML('beforeend', AddListCard.getHtlm);
+    this.contain = this.parentEl.querySelector('.container-for-card');
+    this.listCards = this.parentEl.querySelector(AddListCard.getList);
+    this.titleList = this.parentEl.querySelector(AddListCard.getTitle);
+    this.titleList.textContent = this.title;
+    this.btnAdd = this.contain.querySelector(AddListCard.getbtn);
+    this.btnAdd.addEventListener('click', this.clikcBtn);
+    this.listCards.addEventListener('mouseover', this.mouseOverItem);
+    this.listCards.addEventListener('mouseout', this.mouseOutItem);
+    this.listCards.addEventListener('mousedown', this.mouseDownOnList);
+    this.listCards.addEventListener('mouseup', this.mouseUpOnList);
+  }
+
+  clikcBtn(e) {
+    e.stopPropagation();
+
+    console.log(this.addItem);
+
+    const activeContainer = e.target.closest('.container');
+    const btn = activeContainer.querySelector(AddListCard.getbtn);
+    const changeList = activeContainer.querySelector(AddListCard.getList);
+    if (!this.addItem) {
+      btn.classList.add('active');
+      // const activeList = activeContainer.querySelector(AddListCard.getList);
+      // activeList.insertAdjacentHTML('beforeend',AddListCard.addItem);
+
+      // const newItem = document.createElement('INPUT');
+      // newItem.classList.add('item-list')
+      // newItem.setAttribute('type','text')
+      // activeList.insertAdjacentHTML('beforeend',newItem.outerHTML);
+
+      const inputItem = document.createElement('INPUT');
+      inputItem.classList.add('item-list');
+      inputItem.setAttribute('type', 'text');
+      // changeList.parentNode.insertBefore(newItem,changeList)
+      changeList.insertBefore(inputItem, changeList.lastaChild);
+      inputItem.focus();
+      this.addItem = true;
+      this.actualItem = activeContainer.querySelector('input');
+    } else {
+      console.log('FORM IS NOT FULLED');
+
+      if (!this.actualItem.value) return;
+      const newItem = document.createElement('LI');
+      const newBtnDel = document.createElement('BUTTON');
+
+      newBtnDel.textContent = 'X';
+      newBtnDel.classList.add('delete-item');
+      console.log(newBtnDel);
+      newItem.textContent = this.actualItem.value;
+      newItem.classList.add('item-list');
+      newItem.insertBefore(newBtnDel, newItem.lastaChild);
+      changeList.insertBefore(newItem, changeList.lastaChild);
+
+      btn.classList.remove('active');
+      this.actualItem.remove();
+      this.actualItem = undefined;
+      this.addItem = false;
+
+      // activeList.insertAdjacentHTML('beforeend',newItem_.outerHTML)
+      // newItem.innerHTML = this.actualItem.innerHTML;
+      // this.actualItem.replaceChild(newItem,this.actualItem)
+      // this.actualItem = undefined;
+      // this.addItem = false;
+    }
+
+    // btn.classList.add('active')
+    // // const activeList = activeContainer.querySelector(AddListCard.getList);
+    // // activeList.insertAdjacentHTML('beforeend',AddListCard.addItem);
+
+    // // const newItem = document.createElement('INPUT');
+    // // newItem.classList.add('item-list')
+    // // newItem.setAttribute('type','text')
+    // // activeList.insertAdjacentHTML('beforeend',newItem.outerHTML);
+
+    // const newItem = document.createElement('INPUT');
+    // newItem.classList.add('item-list')
+    // newItem.setAttribute('type','text')
+
+    // btn.parentNode.insertBefore(newItem,btn)
+    // newItem.focus()
+  }
+
+  mouseOutItem() {
+    // e.stopPropagation()
+    if (this.addItem) return;
+    // console.log('OUT-ITem');
+    this.actualItem.classList.remove('active');
+  }
+
+  mouseOverItem(e) {
+    // e.stopPropagation();
+    if (this.addItem) return;
+    if (!e.target.classList.contains('item-list')) return;
+    this.actualItem = e.target;
+    e.target.classList.add('active');
+  }
+
+  mouseDownOnList(e) {
+    // e.stopPropagation();
+    if (e.target.classList.contains('delete-item')) {
+    //   console.log('del');
+      e.target.closest('.item-list').remove();
+    }
+    if (e.target.classList.contains('list')) return;
+    this.actualItem = e.target;
+    // console.log("mouseDown");
+    this.actualItem.classList.remove('active');
+    this.listCards.removeEventListener('mouseout', this.mouseOutItem);
+    this.listCards.removeEventListener('mouseover', this.mouseOverItem);
+
+    console.log('mouseDown');
+  }
+
+  mouseUpOnList() {
+    // e.stopPropagation();
+    // console.log('UP');
+    this.listCards.addEventListener('mouseout', this.mouseOutItem);
+    this.listCards.addEventListener('mouseover', this.mouseOverItem);
+  }
+}
